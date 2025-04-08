03064f8402147d12570ecddc6d0d2977ea05ceef
yuvrajtakk <takcommunity99@gmail.com>
2025-04-08 19:29:19 +0530

    python codes

---
+++ Day 1/task/task.py
@@ -0,0 +1,4 @@
+print("welcome to the Band Name Generator.")
+city=input("whats the city of the name you grow up in?\n")
+pet=input("whats your pet name?\n")
+print("your Band name is "+ city + " "+pet)
