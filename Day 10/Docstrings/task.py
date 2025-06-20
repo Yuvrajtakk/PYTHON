43529f8c1c4f2f04ce482975631024d96351afc0
Yuvraj <takcommunity99@gmail.com>
2025-06-20 21:54:47 +0530

    Merge remote-tracking branch 'github/master'
    # Conflicts:
    #	Day 7/lesson-info.yaml
    #	Day 7/task/main.py
    #	Day 7/task/solution.py

--- Day 10/Docstrings/task.py
+++ Day 10/Docstrings/task.py
@@ -1,4 +1,7 @@
 def format_name(f_name, l_name):
+    """i am trying docstring
+    oh my krishna its fabulous
+    i think i will use this at somewhere.... maybe"""
     formated_f_name = f_name.title()
     formated_l_name = l_name.title()
     return f"{formated_f_name} {formated_l_name}"
